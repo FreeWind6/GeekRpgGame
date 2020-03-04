30d214109508ce73207e16ddb2e7eb83d89c6759
Dmitry <free-wind-2012@yandex.ru>
2020-03-04 22:08:56 +0300

    Add Lesson 3

--- core/src/com/geekbrains/rpg/game/Hero.java
+++ core/src/com/geekbrains/rpg/game/Hero.java
@@ -2,16 +2,15 @@ package com.geekbrains.rpg.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
-import java.util.Random;
-
 public class Hero {
-    private Projectile projectile;
+    private GameScreen gameScreen;
     private TextureRegion texture;
     private TextureRegion texturePointer;
     private TextureRegion textureHp;
@@ -23,33 +22,29 @@ public class Hero {
     private int hp;
     private int hpMax;
     private StringBuilder strBuilder;
-    private Apple apple;
-    private Random randomPosition;
-    private Vector2 randomVector;
 
-    public Hero(TextureAtlas atlas) {
-        this.texture = atlas.findRegion("knight");
-        this.texturePointer = atlas.findRegion("pointer");
-        this.textureHp = atlas.findRegion("hp");
+    public Vector2 getPosition() {
+        return position;
+    }
+
+    public Hero(GameScreen gameScreen) {
+        this.gameScreen = gameScreen;
+        this.texture = Assets.getInstance().getAtlas().findRegion("knight");
+        this.texturePointer = Assets.getInstance().getAtlas().findRegion("pointer");
+        this.textureHp = Assets.getInstance().getAtlas().findRegion("hp");
         this.position = new Vector2(100, 100);
-        this.projectile = new Projectile(atlas);
         this.dst = new Vector2(position);
         this.tmp = new Vector2(0, 0);
         this.speed = 300.0f;
         this.hpMax = 10;
         this.hp = 10;
         this.strBuilder = new StringBuilder();
-        this.apple = new Apple(atlas);
-        this.randomPosition = new Random();
-        randomVector = new Vector2();
     }
 
     public void render(SpriteBatch batch) {
         batch.draw(texturePointer, dst.x - 30, dst.y - 30, 30, 30, 60, 60, 0.5f, 0.5f, lifetime * 90.0f);
         batch.draw(texture, position.x - 30, position.y - 30, 30, 30, 60, 60, 1, 1, 0);
         batch.draw(textureHp, position.x - 30, position.y + 30, 60 * ((float) hp / hpMax), 12);
-        projectile.render(batch);
-        apple.render(batch);
     }
 
     public void renderGUI(SpriteBatch batch, BitmapFont font) {
@@ -60,26 +55,18 @@ public class Hero {
     }
 
     public void update(float dt) {
-        projectile.update(dt);
         lifetime += dt;
         if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
             dst.set(Gdx.input.getX(), 720.0f - Gdx.input.getY());
         }
         if (Gdx.input.isButtonJustPressed(Input.Buttons.RIGHT)) {
-            projectile.setup(position.x, position.y, Gdx.input.getX(), 720.0f - Gdx.input.getY());
+            gameScreen.getProjectilesController().setup(position.x, position.y, Gdx.input.getX(), 720.0f - Gdx.input.getY());
         }
         tmp.set(dst).sub(position).nor().scl(speed); // вектор скорости
         if (position.dst(dst) > speed * dt) {
             position.mulAdd(tmp, dt);
         } else {
             position.set(dst);
-        }
-
-        if (projectile.getPosition().dst(apple.getPosition()) < 15) {
-            int x = randomPosition.nextInt(1000) + 30;
-            int y = randomPosition.nextInt(600) + 30;
-            randomVector.set(x, y);
-            apple.setPosition(randomVector);
         }
     }
 }
--- desktop/src/com/geekbrains/rpg/game/desktop/DesktopLauncher.java
+++ desktop/src/com/geekbrains/rpg/game/desktop/DesktopLauncher.java
@@ -5,11 +5,11 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.geekbrains.rpg.game.GeekRpgGame;
 
 public class DesktopLauncher {
-    public static void main(String[] arg) {
-        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.width = 1280;
-        config.height = 720;
-        config.foregroundFPS = 60;
-        new LwjglApplication(new GeekRpgGame(), config);
-    }
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1280;
+		config.height = 720;
+		config.foregroundFPS = 60;
+		new LwjglApplication(new GeekRpgGame(), config);
+	}
 }
